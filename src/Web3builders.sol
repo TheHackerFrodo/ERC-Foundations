e0581bc25ac82e0f861c926f5dfa8a62fdb626cb
TheHackerFrodo <mitchumdaniel805@gmail.com>
2024-04-19 13:13:17 -0400

    work on the ERC-721 standard

---
+++ src/Web3builders.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.2;
+
+import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract Web3Builders is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
+    uint256 private _nextTokenId;
+    uint256 maxSupply =1;
+
+    bool public publicMintOpen = false;
+    bool public allowListMintOpen = false;
+
+    constructor(address initialOwner)
+        ERC721("Web3Builders", "WE3")
+        Ownable(initialOwner)
+    {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmY5rPqGTN1rZxMQg2ApiSZc7JiBNs1ryDzXPZpQhC1ibm/";
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function editMintWindows(bool _publicMintOpen, bool _allowListMintOpen) external onlyOwner {
+    publicMintOpen = _publicMintOpen;
+    allowListMintOpen = _allowListMintOpen;
+}
+
+
+    // Add public Mnt and AllowListMintOpen Variables
+    function allowListMint() public payable {
+        require(allowListMintOpen, "AllowList Mint Closed");
+        require(msg.value == 0.001 ether, 'not enough ETH');
+        require(totalSupply() < maxSupply, "We Sold Out");
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function publicMint() public payable {
+        require(publicMintOpen, "public mint is closed");
+        require(msg.value == 0.01 ether, 'not enough ETH');
+        require(totalSupply() < maxSupply, "We Sold Out");
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
